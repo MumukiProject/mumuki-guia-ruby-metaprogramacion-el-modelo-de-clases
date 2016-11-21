8c24e1004f792acf4dce4c1b92f9cabcd5b5b77b
franco@mumuki.org <franco@mumuki.org>
2016-11-20 23:07:43 -0500

    Mumuki Export on 2016-11-20 23:07:43 -0500

--- 00008_Sacando estadísticas/test..rb
+++ 00008_Sacando estadísticas/test..rb
@@ -1,3 +1,12 @@
+mancuspias = [
+  {edad: 3, peso: 12}, 
+  {edad: 4.5, peso: 16}, 
+  {edad: 2, peso: 13},
+  {edad: 2, peso: 15},
+  {edad: 1.3, peso: 10},
+  {edad: 6, peso: 18}
+]
+
 /*...content...*/
 
 describe do 
