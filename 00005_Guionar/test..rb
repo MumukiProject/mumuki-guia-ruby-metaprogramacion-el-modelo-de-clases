054464ce3e71497d9c7bf92c25e55e4dd41dd0f0
franco@mumuki.org <franco@mumuki.org>
2016-11-20 21:18:07 -0500

    Mumuki Export on 2016-11-20 21:18:07 -0500

--- 00005_Guionar/test..rb
+++ 00005_Guionar/test..rb
@@ -5,7 +5,30 @@ describe do
     expect(resultado).to eq 'hola-mundo'
   end
   
- 
+  it 'hola mundO' do
+    string_misterioso = 'hola mundO'
+    #...content...#
+    expect(resultado).to eq 'hola-mundo'
+  end
+  
+  it '   hola todo el mundo ' do
+    string_misterioso = '   hola todo el mundo '
+    #...content...#
+    expect(resultado).to eq 'hola-todo-el-mundo'
+  end
+  
+  it 'a la grande le puse Cuca' do
+    string_misterioso = 'a la grande le puse Cuca'
+    #...content...#
+    expect(resultado).to eq 'a-la-grande-le-puse-cuca'
+  end
+  
+  it 'Ahora me lo venis a     decir' do
+    string_misterioso =  'Ahora me lo venis a     decir'
+    #...content...#
+    expect(resultado).to eq 'ahora-me-lo-venis-a-decir'
+  end
+  
   
 end
   
