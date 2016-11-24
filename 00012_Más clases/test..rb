2acde867197c6b927399d6d12d6f87db114a09d5
franco@mumuki.org <franco@mumuki.org>
2016-11-24 11:30:45 -0500

    Mumuki Export on 2016-11-24 11:30:45 -0500

--- 00012_Más clases/test..rb
+++ 00012_Más clases/test..rb
@@ -1,6 +1,11 @@
 describe 'Cuentas' do 
-  let(:cuenta) { Cuenta.new }
+  let(:cuenta) { CuentaConDescubierto.new }
   
-  it 
+  it("si la cuenta tiene $0, hay suficiente descubierto para debitar 300") do 
+    expect(cuenta.suficiente_descubierto? 300).to be true
+  end
   
+  it("si la cuenta tiene $0, NO hay suficiente descubierto para debitar 3000") do 
+    expect(cuenta.suficiente_descubierto? 3000).to be false
+  end
 end
