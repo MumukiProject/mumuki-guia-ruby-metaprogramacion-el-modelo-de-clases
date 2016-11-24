ec5269e50f1559578fda1c897be361b579fd9308
franco@mumuki.org <franco@mumuki.org>
2016-11-24 06:44:46 -0500

    Mumuki Export on 2016-11-24 06:44:46 -0500

--- 00014_Clases abiertas/test..rb
+++ 00014_Clases abiertas/test..rb
@@ -3,7 +3,7 @@ describe 'shout' do
   it("'twist'.shout?") { expect( 'twist'.shout?).to be false }
   it("'hello!'.shout?") { expect( 'hello!'.shout?).to be true }
   
-  it("'hello!'.shout") { expect( 'hello!'.shout).to be 'hello!' }
-  it("'hello'.shout") { expect( 'hello'.shout).to be 'hello!' }
-  it("'guayaquil'.shout") { expect( 'guayaquil'.shout).to be 'guayaquil!' }
+  it("'hello!'.shout") { expect( 'hello!'.shout).to eq 'hello!' }
+  it("'hello'.shout") { expect( 'hello'.shout).to eq 'hello!' }
+  it("'guayaquil'.shout") { expect( 'guayaquil'.shout).to eq 'guayaquil!' }
 end
