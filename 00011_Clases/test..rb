5c0dd9a4e6e82ec74763b9067852b7e436fae766
franco@mumuki.org <franco@mumuki.org>
2016-11-24 06:31:07 -0500

    Mumuki Export on 2016-11-24 06:31:07 -0500

--- 00011_Clases/test..rb
+++ 00011_Clases/test..rb
@@ -1,8 +1,8 @@
 describe 'salero' do 
   
-  it("si el salero tiene 100g, no está lleno") { expect(Salero.new(100).lleno?).to be false  } 
-  it("si el salero tiene 290g, está lleno") { expect(Salero.new(290).lleno?).to be true  } 
-  it("si el salero tiene 270g, está lleno") { expect(Salero.new(270).lleno?).to be true  } 
+  it("si el salero tiene 100g, no esta lleno") { expect(Salero.new(100).lleno?).to be false  } 
+  it("si el salero tiene 290g, esta lleno") { expect(Salero.new(290).lleno?).to be true  } 
+  it("si el salero tiene 270g, esta lleno") { expect(Salero.new(270).lleno?).to be true  } 
   
   it "si el salero tiene 50g, y se derrama, queda vacío" do 
     salero = Salero.new(50)
@@ -10,7 +10,7 @@ describe 'salero' do
     expect(salero.vacio?).to be true
   end
   
-  it "si el salero tiene 200g, y se derrama, NO queda vacío" do 
+  it "si el salero tiene 200g, y se derrama, NO queda vacio" do 
     salero = Salero.new(200)
     salero.derramar!
     expect(salero.vacio?).to be false
